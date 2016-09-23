1cf0f0fde9d0ea3164106805491c365045fb0cb8
zwpdbh <hyperion_z@outlook.com>
2016-09-23 15:22:04 +1200

    < can use date picker

--- bookstore/bookstoreForm.php
+++ bookstore/bookstoreForm.php
@@ -34,51 +34,72 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
 ?>
 
-<style type="text/css">
-    form  { display: table;      }
-    p     { display: table-row;  }
-    label { display: table-cell; }
-    input { display: table-cell; }
-    select{ display: table-cell; }
-</style>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
-<script src="../js/formValidation.js"></script>
+    <style type="text/css">
+        form {
+            display: table;
+        }
 
+        p {
+            display: table-row;
+        }
+
+        label {
+            display: table-cell;
+        }
+
+        input {
+            display: table-cell;
+        }
+
+        select {
+            display: table-cell;
+        }
+    </style>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="../js/formValidation.js"></script>
+    <script>
+        $(document).ready(function () {
+            $( "#date_opened" ).datepicker({
+                changeMonth: true,
+                changeYear: true
+            });
+        });
+    </script>
 <?php
-    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-        ?>
-        <div class="container">
-            <form action="bookstoreForm.php" method="post" id="bookstoreForm">
-                <p>
-                    <label>Bookstore ID</label>
-                    <input type="text" name="storeID" id="storeID" value="<?php echo $storeID; ?>" disabled>
-                </p>
-                <p>
-                    <label>City</label>
-                    <input type="text" name="city" id="city" value="<?php echo $city; ?>">
-                </p>
-                <p>
-                    <label>Address</label>
-                    <input type="text" name="address" id="address" value="<?php echo $address; ?>">
-                </p>
-                <p>
-                    <label>Account Number</label>
-                    <input type="text" name="account" id="account" value="<?php echo $account; ?>">
-                </p>
-                <p>
-                    <label>Open Date</label>
-                    <input type="text" name="date_opened" id="date_opened" value="<?php echo $date_opened; ?>">
-                </p>
-                <p>
-                    <label>Total Salary</label>
-                    <input type="text" name="total_salary" id="total_salary" value="<?php echo $total_salary; ?>" disabled>
-                </p>
-                <p>
-                    <input type="submit" value="Save" id="submit" name="submit" onclick="return=false">
-                </p>
-            </form>
-        </div>
-        <?php
-    } elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        
-    }
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    ?>
+    <div class="container">
+        <form action="bookstoreForm.php" method="post" id="bookstoreForm">
+            <p>
+                <label>Bookstore ID</label>
+                <input type="text" name="storeID" id="storeID" value="<?php echo $storeID; ?>" disabled>
+            </p>
+            <p>
+                <label>City</label>
+                <input type="text" name="city" id="city" value="<?php echo $city; ?>">
+            </p>
+            <p>
+                <label>Address</label>
+                <input type="text" name="address" id="address" value="<?php echo $address; ?>">
+            </p>
+            <p>
+                <label>Account Number</label>
+                <input type="text" name="account" id="account" value="<?php echo $account; ?>">
+            </p>
+            <p>
+                <label>Open Date</label>
+                <input type="text" name="date_opened" id="date_opened" value="<?php echo $date_opened; ?>">
+            </p>
+            <p>
+                <label>Total Salary</label>
+                <input type="text" name="total_salary" id="total_salary" value="<?php echo $total_salary; ?>" disabled>
+            </p>
+            <p>
+                <input type="submit" value="Save" id="submit" name="submit" onclick="return=false">
+            </p>
+        </form>
+    </div>
+    <?php
+} elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+}
--- bookstore/insertBookstore.php
+++ bookstore/insertBookstore.php
@@ -25,6 +25,8 @@
         }
     </style>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <script>
         $(document).ready(function () {
             $('#add').on('click', function () {
